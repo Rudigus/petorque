7d795381485c473d189aeb2f8941c6b43c0d2379
Rudigus <rodrigo.matos9@hotmail.com>
2020-04-20 16:55:10 -0300

    Initializes Xcode project and creates gitignore

---
+++ Petorque/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Petorque
+//
+//  Created by Rodrigo Matos Aguiar on 20/04/20.
+//  Copyright © 2020 Petorqueiros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
