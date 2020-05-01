2da398f8631772779d38093d5fccd1284550aa86
Eduardo Regis <eduregis1@gmail.com>
2020-04-30 21:05:22 -0300

    number of cycles being catched by the edit pickerview

--- Petorque/Controller/EditTaskScheduleViewController.swift
+++ Petorque/Controller/EditTaskScheduleViewController.swift
@@ -9,70 +9,63 @@
 import UIKit
 
 class EditTaskScheduleViewController: UITableViewController {
-    // This variable receives the values of the selected task in the previous screen
     var task: Task?
+    
+    @IBOutlet var nameTextfield: UITextField!
+    
+    @IBOutlet weak var numberOfCyclesTextfield: UITextField!
+    
+    @IBOutlet var cycleDurationContent: CycleDurationPickerView!
+    
+    let numberOfCyclesPicker = UIPickerView()
+    
+    let numberOfCyclesPickerData = [String](arrayLiteral: "1 ciclo", "2 ciclos", "3 ciclos", "4 ciclos", "5 ciclos")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.tableFooterView = UIView()
-        //test.text = task?.title
+        numberOfCyclesPicker.delegate = self
+        numberOfCyclesPicker.selectRow(task!.numberOfCycles - 1, inComponent: 0, animated: true)
+        numberOfCyclesTextfield.inputView = numberOfCyclesPicker
+        numberOfCyclesTextfield.borderStyle = .none
+        if (task?.numberOfCycles == 1) {
+            numberOfCyclesTextfield.text = "1 ciclo"
+        } else {
+            numberOfCyclesTextfield.text = "\(task?.numberOfCycles ?? 0) ciclos"
+        }
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard (_:)))
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    @objc func dismissKeyboard (_ sender: UITapGestureRecognizer) {
+        numberOfCyclesTextfield.resignFirstResponder()
+    }
+    
+    @IBAction func CancelEditTask(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func FinishEditTask(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func DeleteTask(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension EditTaskScheduleViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
 
-    // MARK: - Table view data source
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    func pickerView( _ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return numberOfCyclesPickerData.count
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    func pickerView( _ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+     return numberOfCyclesPickerData[row]
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    func pickerView( _ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        numberOfCyclesTextfield.text = numberOfCyclesPickerData[row]
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
