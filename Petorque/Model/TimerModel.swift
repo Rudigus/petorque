c66ce2869e1551bb6112430cafca68dd36a0f8ba
Silmunia <henriquepcnews@gmail.com>
2020-05-05 19:30:13 -0300

    - Adds timer to Work storyboard - Adds timer play/pause button to Work storyboard - Adds Begin and Finish Task buttons to Work storyboard - Makes timer run on work and break cycles - Makes timer run on cycle duration and number according to current task - Makes timer run counting up or down

---
+++ Petorque/Model/TimerModel.swift
@@ -0,0 +1,106 @@
+//
+//  Timecounter.swift
+//  Petorque
+//
+//  Created by Pedro Henrique Costa on 04/05/20.
+//  Copyright © 2020 Petorqueiros. All rights reserved.
+//
+
+import Foundation
+
+class TimerModel {
+    
+    var timer : Timer?
+    var elapsedSeconds : Int
+    
+    var counterDirection : (work: String, break: String)
+    var remainingCycles : Int
+    var minuteLimit : Int
+    
+    var paused : Bool
+    var working : Bool
+    var delegate : TimerDelegate?
+    
+    init(count type : (work: String, break: String), cycles cyc : Int, minutes min : Int) {
+        self.elapsedSeconds = 0
+        
+        self.counterDirection = type
+        self.remainingCycles = cyc
+        self.minuteLimit = min
+        
+        self.paused = false
+        self.working = true
+    }
+    
+    func startTimer(minutes min : Int) {
+        var timeDirection = ""
+        if working {
+            timeDirection = self.counterDirection.work
+        } else {
+            timeDirection = self.counterDirection.break
+        }
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { timer in
+            let minutes = self.elapsedSeconds / 60
+            let seconds = self.elapsedSeconds - (60 * minutes)
+            var timeText : String = ""
+            
+            self.elapsedSeconds += 1
+            
+            if timeDirection == "Prog" {
+                timeText = "\(minutes):\(seconds)"
+                if minutes >= min {
+                    self.stopTimer(reset: true)
+                }
+            } else {
+                if seconds == 0 {
+                    timeText = "\((60 * min - self.elapsedSeconds) / 60):\(seconds)"
+                } else {
+                    timeText = "\((60 * min - self.elapsedSeconds) / 60):\(60 - seconds)"
+                }
+                
+                if self.elapsedSeconds >= 60 * min {
+                    self.stopTimer(reset: true)
+                }
+            }
+            
+            print(timeText)
+            self.delegate?.giveTimerLabel(timeText)
+        }
+        timer?.fire()
+    }
+    
+    func pauseTimer() {
+        if paused {
+            if working {
+                startTimer(minutes: minuteLimit)
+            } else {
+                startTimer(minutes: 5)
+            }
+        } else {
+            stopTimer()
+        }
+        paused.toggle()
+    }
+    
+    func stopTimer(reset res : Bool = false) {
+        timer?.invalidate()
+        timer = nil
+        if res {
+            elapsedSeconds = 0
+            
+            if working {
+                remainingCycles -= 1
+                if remainingCycles > 0 {
+                    working.toggle()
+                    startTimer(minutes: 5)
+                } else {
+                    self.delegate?.finishedTask()
+                }
+            } else {
+                working.toggle()
+                startTimer(minutes: minuteLimit)
+            }
+        }
+    }
+}
---
+++ Petorque/Protocols/TimerDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  TimerDelegate.swift
+//  Petorque
+//
+//  Created by Pedro Henrique Costa on 05/05/20.
+//  Copyright © 2020 Petorqueiros. All rights reserved.
+//
+
+import Foundation
+
+protocol TimerDelegate {
+    
+    func giveTimerLabel(_ timeText : String)
+    
+    func finishedTask()
+    
+}
