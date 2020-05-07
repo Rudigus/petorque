56d1ce1035e7dfe9437633297c6db32792b5489c
Rudigus <rodrigo.matos9@hotmail.com>
2020-05-07 17:35:55 -0300

    trying new things on schedule

--- Petorque/Controller/ScheduleController.swift
+++ Petorque/Controller/ScheduleController.swift
@@ -35,11 +35,16 @@ class ScheduleController: UIViewController, UITableViewDelegate, UITableViewData
     //Using this method to call a custom function for delegating the TableView's delegate and it's data source
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
+        setupDayControl()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         allTasks = Database.shared.loadData(from: .doing)
         tasks = loadTodayTasks()
         messageIsHidden()
-        setupTableView()
-        setupDayControl()
+        
     }
     
     func setupDayControl()
